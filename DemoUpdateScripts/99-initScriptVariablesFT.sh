33048b2182983f01431030c217ed49ef601e4d99
Fred Vadon <fv@nuxeo.com>
2013-08-12 12:20:58 +0200

    Update FT_en to 5.7.2

--- DemoUpdateScripts/99-initScriptVariablesFT.sh
+++ DemoUpdateScripts/99-initScriptVariablesFT.sh
@@ -25,7 +25,7 @@ export NUXEO_DEMO_PACKAGES=${NUXEO_DEMO_PACKAGES:-packages}
 #default packages to remove separated by spaces, ex :nuxeo-cmf nuxeo-social-collaboration
 export NUXEO_DEFAULT_PACKAGE_TO_REMOVE=${NUXEO_DEFAULT_PACKAGE_TO_REMOVE:- nuxeo-cmf}
 
-export NUXEO_DISTRIB_DOWNLOAD_LINK=${NUXEO_DISTRIB_DOWNLOAD_LINK:-http://community.nuxeo.com/static/releases/nuxeo-5.7.1/}
+export NUXEO_DISTRIB_DOWNLOAD_LINK=${NUXEO_DISTRIB_DOWNLOAD_LINK:-http://community.nuxeo.com/static/releases/nuxeo-5.7.2/}
 
 export NUXEO_CONF=${NUXEO_CONF:-/etc/nuxeo/nuxeo.conf}
 export NUXEO_SERVER_DIR=${NUXEO_SERVER_DIR:-/var/lib/nuxeo/server}
