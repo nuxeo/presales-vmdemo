4ae184d5845a6788a79a91822f2520a542863cbf
Michaël Vachette <mvachette@nuxeo.com>
2018-09-25 10:34:11 -0700

    Remove obsolete scripts

--- MaintenanceScripts/resetAndUpdateGeneric.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script file will update the data of the demo and then reset it. The demo path should be updated automatically when the demo is created.
-
-cd /etc/nuxeo_snapshot_demo/scripts
-./01-updateFullDemoFromFromWeb.sh
-./000-resetLocal.sh
-
--- MaintenanceScripts/resetGeneric.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This script file will launch the reset of the snapshot demo. It should be downloaded and updated automatically by SnapshotDemo
-
-cd /etc/nuxeo_snapshot_demo/scripts
-./000-resetLocal.sh
