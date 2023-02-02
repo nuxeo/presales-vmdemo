f8a7cedadff00671e100a71ce7147748ecec5896
michael <5880528+michaelva@users.noreply.github.com>
2023-02-02 13:38:28 +0900

    Update AMI and install script

--- AMI-builder/setup.sh
+++ AMI-builder/setup.sh
@@ -30,12 +30,20 @@ pip3 install -q awscli --upgrade
 export PATH=$PATH:~/.local/bin/
 
 # Install docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get -q -y remove docker docker-engine docker.io
-apt-get -q -y install docker-ce
-pip3 install -q --system docker-compose
+apt-get -q -y remove docker docker-engine docker.io containerd runc
+apt-get -q -y update
+apt-get -q -y install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get -q -y update
+apt-get -q -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 usermod -aG docker ubuntu
 
 # Install Certbot
